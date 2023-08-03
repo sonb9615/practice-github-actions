01692d94bb0d25d1dbede2931b77f1ce213f1f36
sonb9615 <sonb9615@gmail.com>
2023-08-03 20:34:39 +0900

    [23.08.03] init

---
+++ practice-github-actions/src/main/java/com/practice/practicegithubactions/PracticeGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.practice.practicegithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeGithubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeGithubActionsApplication.class, args);
+	}
+
+}
---
+++ practice-github-actions/src/test/java/com/practice/practicegithubactions/PracticeGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.practicegithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
